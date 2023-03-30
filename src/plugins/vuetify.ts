c5a974d26c175b2306906c506210e122d88f1cef
Noah <noahward@shaw.ca>
2023-03-30 14:56:01 -0400

    Add and configure vuetify

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,29 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import '@mdi/font/css/materialdesignicons.css'
+
+export default createVuetify({
+  components,
+  directives,
+
+  theme: {
+    defaultTheme: 'dark'
+  },
+  defaults: {
+    VCard: {
+      rounded: 'md'
+    },
+    VTextField: {
+      variant: 'outlined',
+      density: 'comfortable',
+      color: 'primary'
+    },
+    VSelect: {
+      variant: 'outlined',
+      density: 'comfortable',
+      color: 'primary'
+    }
+  }
+})
