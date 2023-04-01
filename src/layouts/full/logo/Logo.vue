f954d7b3b9db4cda035fe02a21f0b83a18498dcd
Noah <noahward@shaw.ca>
2023-04-01 14:18:52 -0400

    Change logo to svg

--- src/layouts/full/logo/Logo.vue
+++ src/layouts/full/logo/Logo.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useTheme } from 'vuetify'
+import LogoDark from './LogoDark.vue'
+import LogoLight from './LogoLight.vue'
 
 const theme = useTheme()
-const isDark = computed(() => {
-  if (theme.global.current.value.dark) {
-    return 'light'
-  }
-  return 'dark'
-})
 </script>
 
 <template>
-  <div class="logo">
-    <RouterLink to="/">
-      <v-img
-        width="200"
-        :src="`src/assets/images/logos/logo-${isDark}-sm.png`"
-        alt="SmartSpend"
-      />
-    </RouterLink>
+  <div>
+    <LogoLight v-if="theme.global.current.value.dark" />
+    <LogoDark v-else />
   </div>
 </template>
