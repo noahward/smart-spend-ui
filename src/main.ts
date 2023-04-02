4d07853857cbf37ce5affdf92fd73ebdde64121a
Noah <noahward@shaw.ca>
2023-04-02 10:08:23 -0400

    Add perfect-scrollbar package to project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from '@/router'
+import PerfectScrollbar from 'vue3-perfect-scrollbar'
 import App from './App.vue'
 import vuetify from '@/plugins/vuetify'
 
@@ -10,6 +11,7 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(PerfectScrollbar)
 app.use(createPinia())
 
 app.mount('#app')
