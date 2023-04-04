58d645246339ded8c4fd535f77bb6ff8aa6e47ae
Noah <noahward@shaw.ca>
2023-04-03 23:03:55 -0400

    Configure auth interceptors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,33 @@
 import axios from 'axios'
+import { router } from '@/router'
+import { useAuthStore } from '@/stores/auth'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
 })
+
+api.interceptors.request.use(
+  config => {
+    const authStore = useAuthStore()
+    if (authStore.isAuthenticated) {
+      config.headers.Authorization = `Token ${authStore.user.token.key}`
+    }
+    return config
+  },
+  error => { return Promise.reject(error) }
+)
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 401 && router.currentRoute.value.meta.requiresAuth) {
+      localStorage.removeItem('user')
+      router.push('/login')
+      return Promise.reject(error)
+    }
+    return Promise.reject(error)
+  }
+)
 
 if (import.meta.env.VITE_LOG_REQUESTS === 'true') {
   api.interceptors.request.use(request => {
