e40e4467340e15435a7f709ed396a632169ca9d3
Noah <noahward@shaw.ca>
2023-04-09 10:36:04 -0400

    Create table for previewing a file's transactions

---
+++ src/components/shared/PreviewTable.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { formatDate } from '@/helpers/formatDate'
+import { formatCurrency } from '@/helpers/formatCurrency'
+import type { AccountTransactionsPreview } from '@/types/file-preview'
+
+type PropTypes = { previewData: AccountTransactionsPreview }
+
+const props = defineProps<PropTypes>()
+
+const orderedTransactions = computed(() => {
+  const transactionsCopy = props.previewData.transactions.map(itm => { return { ...itm } })
+  return transactionsCopy.sort((a, b) => {
+    return new Date(b.date).valueOf() - new Date(a.date).valueOf()
+  })
+})
+
+const headers = ref([
+  { title: 'Date', key: 'date' },
+  { title: 'Description', key: 'description' },
+  { title: 'Amount', key: 'amount' }
+])
+</script>
+
+<template>
+  <v-data-table
+    density="compact"
+    :headers="headers"
+    :items="orderedTransactions"
+    :items-per-page="5"
+  >
+    <template #item="{ item }">
+      <tr>
+        <td>{{ formatDate(item.columns.date) }}</td>
+        <td>{{ item.columns.description }}</td>
+        <td>
+          {{ formatCurrency(item.columns.amount, item.columns.currencyCode) }}
+        </td>
+      </tr>
+    </template>
+  </v-data-table>
+</template>
