46b5f2405e0c44c61040894b44c96fca8ae622d9
Noah <noahward@shaw.ca>
2023-04-11 22:17:16 -0400

    Add information cards to dashboard

---
+++ src/components/dashboard/TopCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { formatCurrency } from '@/helpers/formatCurrency'
+import type { TablerIconComponent } from 'vue-tabler-icons'
+
+type PropTypes = {
+  icon: TablerIconComponent;
+  title: string;
+  value: number;
+  number?: boolean;
+}
+
+withDefaults(defineProps<PropTypes>(), { number: false })
+</script>
+
+<template>
+  <div class="d-flex flex-column align-center justify-center text-center rounded-md pa-6 bg-lightSecondary h-100">
+    <component
+      :is="icon"
+      size="40"
+    />
+    <div class="text-h6 text-capitalize font-weight-bold mt-3">
+      {{ title }}
+    </div>
+    <div
+      class="text-h4 mt-4"
+      :class="value >= 0 ? 'text-success' : 'text-error'"
+    >
+      <span v-if="number">{{ formatCurrency(value) }}</span>
+      <span v-else>{{ value }}</span>
+    </div>
+  </div>
+</template>
