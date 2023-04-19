40d17c7176751ed53deec722d048198529620776
Noah <noahward@shaw.ca>
2023-04-18 21:47:27 -0400

    Fix footer background color width

--- src/components/landing/Footer.vue
+++ src/components/landing/Footer.vue
@@ -2,14 +2,16 @@
 </script>
 
 <template>
-  <v-container class="d-flex justify-center align-center bg-surface">
-    <div class="d-flex align-center">
-      <img
-        src="@/assets/images/logos/footer-logo.png"
-        alt="landing-background"
-        class="footer-img mr-3"
-      >
-      <span class="text-13 text-textSecondary">© 2023 SmartSpend, Inc. All rights reserved.</span>
-    </div>
-  </v-container>
+  <div class="bg-surface">
+    <v-container class="d-flex justify-center align-center">
+      <div class="d-flex align-center">
+        <img
+          src="@/assets/images/logos/footer-logo.png"
+          alt="landing-background"
+          class="footer-img mr-3"
+        >
+        <span class="text-13 text-textSecondary">© 2023 SmartSpend, Inc. All rights reserved.</span>
+      </div>
+    </v-container>
+  </div>
 </template>
