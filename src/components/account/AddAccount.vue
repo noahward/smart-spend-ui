e390b233450e9dfcd8735131a9f619f5eaba20b5
Noah <noahward@shaw.ca>
2023-04-30 15:06:50 -0400

    Submit currency code with account creation

--- src/components/account/AddAccount.vue
+++ src/components/account/AddAccount.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import currencies from 'iso4217'
 import { Form } from 'vee-validate'
 import { camelizeKeys } from 'humps'
 import { object, string, number } from 'yup'
+import { getSubmitFn } from '@/helpers/validationHelper'
 import { useAccountStore } from '@/stores/account'
 import ValidatedInputField from '@/components/shared/validators/ValidatedInputField.vue'
 import ValidatedSelectField from '@/components/shared/validators/ValidatedSelectField.vue'
+import ValidatedAutocompleteField from '@/components/shared/validators/ValidatedAutocompleteField.vue'
 import type { AccountAPIErrors } from '@/types/account'
 
 const emit = defineEmits(['inFocus', 'closeDialog'])
@@ -13,13 +16,18 @@ const emit = defineEmits(['inFocus', 'closeDialog'])
 const accountSchema = object({
   name: string().required('Add a name for your account'),
   kind: string().required('Select an account type'),
+  currencyCode: string().required('Select an account currency'),
   initialBalance: number().typeError('Balance must be a number').required('Enter the account starting balance')
 })
 
 const accountStore = useAccountStore()
-const createErrors = ref<AccountAPIErrors>()
+const createErrors = ref<AccountAPIErrors>({})
 
-function onSubmit (values: object) {
+const onSubmit = getSubmitFn(accountSchema, (values: any) => {
+  if (currencyOptions.indexOf(values.currencyCode) === -1) {
+    createErrors.value.currencyCode = ['Invalid currency code']
+    return
+  }
   accountStore.createAccount(values)
     .then(() => {
       emit('closeDialog')
@@ -27,12 +35,17 @@ function onSubmit (values: object) {
     .catch((error) => {
       createErrors.value = camelizeKeys(error.response.data)
     })
-}
+})
 
 const accountOptions = [
   { title: 'Spending', value: 'spending' },
   { title: 'Saving', value: 'saving' }
 ]
+
+const currencyOptions = Object.keys(currencies)
+  .map(key => currencies[key].Code)
+  .filter(code => code && code.length === 3)
+  .sort()
 </script>
 
 <template>
@@ -55,6 +68,14 @@ const accountOptions = [
           label="Type"
           :options="accountOptions"
           :errors="createErrors?.kind"
+        />
+      </div>
+      <div class="my-2">
+        <ValidatedAutocompleteField
+          name="currencyCode"
+          label="Currency"
+          :options="currencyOptions"
+          :errors="createErrors?.currencyCode"
         />
       </div>
       <div class="my-2">
--- src/types/account.ts
+++ src/types/account.ts
@@ -2,6 +2,7 @@ export type Account = {
   id: number;
   name: string;
   kind: 'saving' | 'spending';
+  currencyCode: string;
   balance: number;
   initialBalance: number;
 }
@@ -10,12 +11,14 @@ export type AccountUpdate = {
   id: number;
   name?: string;
   kind?: 'saving' | 'spending';
+  currencyCode?: string;
   balance?: number;
 }
 
 export type AccountAPIErrors = {
   name?: Array<string>;
   kind?: Array<string>;
+  currencyCode?: Array<string>;
   initialBalance?: Array<string>;
   balance?: Array<string>;
   nonFieldErrors?: Array<string>;
