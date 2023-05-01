65fe145d651a14303aff3a68d80e637afe0922fc
Noah <noahward@shaw.ca>
2023-04-30 22:35:50 -0400

    Choose date of forex when uploading transactions

--- src/components/transaction/UploadTransactions.vue
+++ src/components/transaction/UploadTransactions.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import { ExclamationCircleIcon } from 'vue-tabler-icons'
+import { ExclamationCircleIcon, HelpIcon } from 'vue-tabler-icons'
 import { useAccountStore } from '@/stores/account'
 import PreviewTable from '@/components/transaction/PreviewTable.vue'
 import type { AccountTransactionsPreview } from '@/types/file-preview'
@@ -10,8 +10,13 @@ type PropTypes = {
   accountName?: string
 }
 
+type AccountMapData = {
+  targetAccountId: number,
+  exchangeDate: 'today' | 'transaction'
+}
+
 type AccountMap = {
-  [key: string]: number
+  [key: string]: AccountMapData
 }
 
 const emit = defineEmits(['closeDialog', 'submitMap'])
@@ -23,6 +28,29 @@ const step = ref(0)
 const accountMap = ref<AccountMap>({})
 const accountError = ref<string | null>()
 const selectedAccount = ref<string | null>(props.accountName || null)
+const handleExchangeRate = ref<'today' | 'transaction'>('today')
+
+const forexNeeded = computed(() => {
+  const currencyArr: string[] = []
+
+  props.previewData[step.value].transactions.forEach((transaction) => {
+    if (!currencyArr.includes(transaction.currencyCode)) {
+      currencyArr.push(transaction.currencyCode)
+    }
+  })
+
+  const targetAcc = accounts.find(acc => acc.name === selectedAccount.value)
+  if (!targetAcc) {
+    return true
+  }
+
+  if (currencyArr.length === 1) {
+    if (currencyArr[0].toUpperCase() === targetAcc.currencyCode.toUpperCase()) {
+      return false
+    }
+  }
+  return true
+})
 
 const isMultistep = computed(() => {
   return props.previewData.length > 1
@@ -43,16 +71,22 @@ function nextStep () {
     return
   }
 
-  accountMap.value[props.previewData[step.value].id] = targetAcc.id
+  accountMap.value[props.previewData[step.value].id] = {
+    targetAccountId: targetAcc.id,
+    exchangeDate: handleExchangeRate.value
+  }
+
   accountError.value = null
   selectedAccount.value = null
+  handleExchangeRate.value = 'today'
   step.value += 1
 }
 
 function previousStep () {
   step.value -= 1
-  const targetAcc = accounts.find(acc => acc.id === accountMap.value[props.previewData[step.value].id])
+  const targetAcc = accounts.find(acc => acc.id === accountMap.value[props.previewData[step.value].id].targetAccountId)
   selectedAccount.value = targetAcc ? targetAcc.name : null
+  handleExchangeRate.value = accountMap.value[props.previewData[step.value].id].exchangeDate
   accountError.value = null
 }
 
@@ -67,9 +101,13 @@ function onSubmit () {
     return
   }
 
-  accountMap.value[props.previewData[step.value].id] = targetAcc.id
+  accountMap.value[props.previewData[step.value].id] = {
+    targetAccountId: targetAcc.id,
+    exchangeDate: handleExchangeRate.value
+  }
   accountError.value = null
   selectedAccount.value = null
+  handleExchangeRate.value = 'today'
   emit('submitMap', accountMap.value)
 }
 </script>
@@ -108,6 +146,43 @@ function onSubmit () {
       </div>
     </div>
 
+    <div v-if="forexNeeded">
+      <div class="d-flex align-center exch-container">
+        <span class="text-13 text-textMedium text-weight-bold mr-2">What date would you like to use for exchange rates?</span>
+        <v-btn flat>
+          <help-icon
+            size="16"
+            class="text-primary pointer"
+          />
+          <v-tooltip
+            max-width="300"
+            activator="parent"
+            location="top"
+          >
+            The transaction currencies contained in your uploaded file do not
+            match the selected account's base currency. Transactions will be
+            exchanged to the selected account's base currency.
+          </v-tooltip>
+        </v-btn>
+      </div>
+      <v-radio-group
+        v-model="handleExchangeRate"
+        class="text-textPrimary"
+      >
+        <v-radio
+          label="Today's date"
+          value="today"
+          color="primary"
+        />
+        <v-radio
+          label="Transaction date"
+          value="transaction"
+          color="primary"
+          class="my-n2"
+        />
+      </v-radio-group>
+    </div>
+
     <div class="my-2">
       <v-card
         variant="outlined"
@@ -120,7 +195,7 @@ function onSubmit () {
       <v-btn
         v-if="isMultistep && step !== previewData.length - 1"
         flat
-        color="textMedium"
+        color="primary"
         @click="nextStep"
       >
         Next
@@ -153,3 +228,12 @@ function onSubmit () {
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.exch-container > .v-btn {
+  border-radius: 25px !important;
+  height: 16px !important;
+  min-width: 0px !important;
+  padding: 0px !important
+}
+</style>
