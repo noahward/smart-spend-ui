970259fe777c466edf86ab9405deea311c72cfbf
Noah <noahward@shaw.ca>
2023-05-09 21:23:59 -0400

    Dashboard uses normalized transaction currencies

--- src/views/main/Dashboard.vue
+++ src/views/main/Dashboard.vue
@@ -1,18 +1,53 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useExchRateStore } from '@/stores/exchange-rate'
 import { useTransactionStore } from '@/stores/transaction'
 import TopCardContainer from '@/components/dashboard/TopCardContainer.vue'
 import ChartSavingsByMonth from '@/components/dashboard/charts/ChartSavingsByMonth.vue'
 import ChartTransactionCategories from '@/components/dashboard/charts/ChartTransactionCategories.vue'
+import type { Transaction } from '@/types/transaction'
+import type { ExchangeRate } from '@/types/exchange-rate'
 
 const transactionStore = useTransactionStore()
+const exchangeRateStore = useExchRateStore()
+const baseCurrency = ref('USD')
+const loading = ref(false)
+const convertedTransactions = ref<Transaction[]>([])
 
 onMounted(() => {
+  loading.value = true
   transactionStore.getTransactions()
+    .then(async () => {
+      await updateExchangeRates(transactionStore.transactions)
+      convertedTransactions.value = normalizeTransactionCurrencies(transactionStore.transactions, exchangeRateStore.exchangeRates)
+    })
     .catch((error) => {
       console.error(error)
     })
+    .finally(() => {
+      loading.value = false
+    })
 })
+
+function updateExchangeRates (transactions: Transaction[]) {
+  const distinctCurrencyCodes = [...new Set(transactions.map(obj => obj.currencyCode))]
+  const promises: Promise<any>[] = []
+  distinctCurrencyCodes.forEach(code => {
+    promises.push(exchangeRateStore.getExchangeRate(code, baseCurrency.value))
+  })
+  return Promise.all(promises)
+}
+
+function normalizeTransactionCurrencies (transactions: Transaction[], exchangeRates: ExchangeRate[]) {
+  return transactions.map((transaction) => {
+    const exchangeObj = exchangeRates.find((rate) => rate.from === transaction.currencyCode)
+    if (exchangeObj) {
+      transaction.amount = transaction.amount * exchangeObj.exchangeRate
+      transaction.currencyCode = exchangeObj.to
+    }
+    return transaction
+  })
+}
 </script>
 
 <template>
