a68c88aca164e6ee7090696cdabc89d09e24bf0e
Noah <noahward@shaw.ca>
2023-05-11 08:16:35 -0400

    Add account from dashboard if none exist

--- src/views/main/Dashboard.vue
+++ src/views/main/Dashboard.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import { PlusIcon, UploadIcon } from 'vue-tabler-icons'
 import { useExchRateStore } from '@/stores/exchange-rate'
+import { useAccountStore } from '@/stores/account'
 import { useTransactionStore } from '@/stores/transaction'
+import CardBase from '@/components/shared/CardBase.vue'
+import AddAccount from '@/components/account/AddAccount.vue'
 import TopCardContainer from '@/components/dashboard/TopCardContainer.vue'
 import ChartSavingsByMonth from '@/components/dashboard/charts/ChartSavingsByMonth.vue'
 import ChartTransactionCategories from '@/components/dashboard/charts/ChartTransactionCategories.vue'
 import type { Transaction } from '@/types/transaction'
-import type { ExchangeRate } from '@/types/exchange-rate'
 
+const accountStore = useAccountStore()
 const transactionStore = useTransactionStore()
 const exchangeRateStore = useExchRateStore()
-const baseCurrency = ref('USD')
+
 const loading = ref(false)
+const baseCurrency = ref('USD')
+const dialogAccount = ref(false)
 const convertedTransactions = ref<Transaction[]>([])
+
+function updateBaseCurrency (newBaseCurrency: string) {
+  loading.value = true
+  baseCurrency.value = newBaseCurrency
+  convertTransactionCurrencies(transactionStore.transactions, baseCurrency.value)
+  loading.value = false
+}
 
 onMounted(() => {
   loading.value = true
   transactionStore.getTransactions()
     .then(async () => {
-      await updateExchangeRates(transactionStore.transactions)
-      convertedTransactions.value = normalizeTransactionCurrencies(transactionStore.transactions, exchangeRateStore.exchangeRates)
+      convertedTransactions.value = convertTransactionCurrencies(transactionStore.transactions, baseCurrency.value)
     })
     .catch((error) => {
       console.error(error)
@@ -29,18 +41,11 @@ onMounted(() => {
     })
 })
 
-function updateExchangeRates (transactions: Transaction[]) {
-  const distinctCurrencyCodes = [...new Set(transactions.map(obj => obj.currencyCode))]
-  const promises: Promise<any>[] = []
-  distinctCurrencyCodes.forEach(code => {
-    promises.push(exchangeRateStore.getExchangeRate(code, baseCurrency.value))
-  })
-  return Promise.all(promises)
-}
+function convertTransactionCurrencies (transactions: Transaction[], baseCurrency: string) {
+  refreshExchangeRates(transactions, baseCurrency)
 
-function normalizeTransactionCurrencies (transactions: Transaction[], exchangeRates: ExchangeRate[]) {
   return transactions.map((transaction) => {
-    const exchangeObj = exchangeRates.find((rate) => rate.from === transaction.currencyCode)
+    const exchangeObj = exchangeRateStore.exchangeRates.find((rate) => rate.from === transaction.currencyCode)
     if (exchangeObj) {
       transaction.amount = transaction.amount * exchangeObj.exchangeRate
       transaction.currencyCode = exchangeObj.to
@@ -48,29 +53,116 @@ function normalizeTransactionCurrencies (transactions: Transaction[], exchangeRa
     return transaction
   })
 }
+
+function refreshExchangeRates (transactions: Transaction[], baseCurrency: string) {
+  const distinctCurrencyCodes = [...new Set(transactions.map(obj => obj.currencyCode))]
+  const promises: Promise<any>[] = []
+  distinctCurrencyCodes.forEach(code => {
+    promises.push(exchangeRateStore.getExchangeRate(code, baseCurrency))
+  })
+  return Promise.all(promises)
+}
+
 </script>
 
 <template>
   <div class="dashboard">
-    <TopCardContainer class="hidden-sm-and-down" />
-    <v-row>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <ChartTransactionCategories kind="inflow" />
-      </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <ChartTransactionCategories kind="outflow" />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <ChartSavingsByMonth />
-      </v-col>
-    </v-row>
+    <TopCardContainer
+      :transactions="convertedTransactions"
+      :loading="loading"
+      class="hidden-sm-and-down"
+      @update-base-currency="updateBaseCurrency"
+    />
+    <div
+      v-if="transactionStore.transactions.length === 0"
+      class="d-flex justify-center align-center flex-column my-10"
+    >
+      <span class="text-textPrimary text-h5">No insights to display!</span>
+      <span class="text-textPrimary text-14">Come back once you've added a few transactions.</span>
+      <div class="mt-5">
+        <v-btn
+          v-if="accountStore.accounts.length === 0"
+          flat
+          color="primary"
+          @click="dialogAccount = true"
+        >
+          <template #prepend>
+            <PlusIcon
+              size="18"
+              class="text-textPrimary"
+            />
+          </template>
+          Add Account
+        </v-btn>
+        <div v-else>
+          <v-btn
+            flat
+            color="primary"
+            class="mr-2"
+            to="/dashboard"
+          >
+            <template #prepend>
+              <PlusIcon
+                size="18"
+                class="text-textPrimary"
+              />
+            </template>
+            Add Transaction
+          </v-btn>
+          <v-btn
+            flat
+            color="primary"
+            class="ml-2"
+            to="/dashboard"
+          >
+            <template #prepend>
+              <UploadIcon
+                size="18"
+                class="text-textPrimary"
+              />
+            </template>
+            Upload Transactions
+          </v-btn>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <ChartTransactionCategories kind="inflow" />
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <ChartTransactionCategories kind="outflow" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <ChartSavingsByMonth />
+        </v-col>
+      </v-row>
+    </div>
+
+    <v-dialog
+      v-model="dialogAccount"
+      width="375"
+      persistent
+    >
+      <CardBase>
+        <template #header>
+          <v-card-title class="text-h5">
+            Add Account
+          </v-card-title>
+        </template>
+        <template #content>
+          <AddAccount @close-dialog="dialogAccount = false" />
+        </template>
+      </CardBase>
+    </v-dialog>
   </div>
 </template>
